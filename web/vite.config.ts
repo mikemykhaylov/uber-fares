5d4316e8f3510817fe4076a7510f3fa5e2509b0b
mmykhaylov <mikemikhailov2003@gmail.com>
2022-05-02 20:05:11 -0700

    Rewrote the web scaffolding
    - Base setup now comes from create-vite
    - Migrated to Yarn 3 for future compatibility
    - Moved ignored web files to web/.gitignore
    - Removed unused files (.eslintignore, LICENSE, old README.md)
    - Fixed csstype dependency issues from 20516173

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import checker from 'vite-plugin-checker';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-        typescript: true, eslint: {
-          lintCommand: "eslint -c ./.eslintrc.yml \"./src/**/*.ts?(x)\"" // for example, lint .ts & .tsx
-        },
-      }
-    ),
-  ]
-});
+  plugins: [react()]
+})
